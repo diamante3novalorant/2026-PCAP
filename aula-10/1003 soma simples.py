304834f35bcbeba537ba25d5748c83b27f9943a8
diamante3novalorant <20261ctb0100049@estudantes.ifpr.edu.br>
2026-06-06 00:22:49 -0300

    Update 1003 soma simples.py

--- aula-10/1003 soma simples.py
+++ aula-10/1003 soma simples.py
@@ -1,1 +1,22 @@
+'''
+Problema: Beecrowd - 1003
+Data: 2026/03/07
+Estudante: Rodrigo Magagnin
+'''
+#Objetivo: Ler dois inteiros em variáveis A e B e calcular a soma 
 
+#--- Análise (LIAC) ---
+#Entrada: dois numeros inteiros, um em cada linha
+#Processamento: A soma de A e B e armazenar em SOMA
+#Saída: Exibir a soma entre os dois valores no formato "SOMA = valor"
+
+#int() converte o texto lido para número inteiro
+#input() lê o valor fornecido
+#int(input()) lê e converte em uma única instrução
+#print() é o comando de saída em Python
+
+A = int(input())
+B = int(input())
+SOMA = A+B
+print(f"SOMA = {SOMA}" )
+ 
