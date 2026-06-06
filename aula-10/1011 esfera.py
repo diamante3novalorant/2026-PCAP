50df4dfd691729ef96c733aadb97059a5f7675b6
diamante3novalorant <20261ctb0100049@estudantes.ifpr.edu.br>
2026-06-06 00:22:02 -0300

    Update 1011 esfera.py

--- aula-10/1011 esfera.py
+++ aula-10/1011 esfera.py
@@ -1,1 +1,26 @@
+'''
+Problema: beecrowd 1011
+Data: 2026.04.07
+Estudante: Rodrigo Magagnin
+'''
+#Objetivo:Ler o raio de uma esfera e calcular seu volume com a formula (4/3) * pi *R³
 
+# --- ANÁLISE (LIAC) ---
+#Entrada: um número de ponto flutuante (o raio R)
+#Processamento: aplicar a fórmula do volume da esfera
+#Saída: exibir no formato "VOLUME = valor" com 3 casa decimais
+#float() converte o valor lido para número decimal (ponto flutuante)
+R = float(input())
+
+# O enunciado pede para atribuir pi como constante
+pi = 3.14159
+
+#4.0/3 garante divisão decimal (não inteira)
+# R**3 R ao cubo R³
+V = (4.0 / 3) *pi * R ** 3
+
+# :.3f formata com exatas 3 casas decimais
+
+print(f"VOLUME = {V:.3f}")
+
+ 
