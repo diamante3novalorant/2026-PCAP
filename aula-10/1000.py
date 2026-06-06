689326d5464e43dd685aec881c918734985db504
diamante3novalorant <20261ctb0100049@estudantes.ifpr.edu.br>
2026-06-06 00:06:20 -0300

    Update 1000.py

--- aula-10/1000.py
+++ aula-10/1000.py
@@ -1,1 +1,15 @@
+'''
+Problema: Beecrowd - 1000
+Data: 2026/03/07
+Estudante: Rodrigo Magagnin
+'''
+#Objetivo: Exibir a Mensagem "Hello World!" Na tela
+
+#--- Análise (LIAC) ---
+#Entrada: Nenhuma
+#Processamento: Nenhum
+#Saída: Exibir o texto "Hello World!"
+
+#print() é o comando de saída no Python
+print('Hello World!')
 
