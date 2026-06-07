2148c5a06660906ac6d460dd5ce198917421552e
diamante3novalorant <20261ctb0100049@estudantes.ifpr.edu.br>
2026-06-07 16:56:33 -0300

    Update 1051 imposto de renda.py

--- aula-14/1051 imposto de renda.py
+++ aula-14/1051 imposto de renda.py
@@ -1,1 +1,29 @@
+'''
+Problema: beecrowd 1051
+Data: 2026.05.14
+Estudante: Rodrigo Magagnin
+'''
 
+#Objetivo: ler um valor com duas casas decimais e calcular o valor que essa pessoa deve pagar de imposto de renda
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: Um valor com duas casas decimais
+# Processamento: calcular as porcentagens do salário do pobre cidadão que vão para o governo
+# Saída: deve ser "R$ (x)"
+
+A = float(input())
+B = 0
+C = A
+if A > 4500:
+    B += ((A - 4500) * 0.28)
+    C = (C - (A - 4500))
+if A > 3000:
+    B += ((C - 3000) * 0.18)
+    C = (C - (C - 3000))
+if A > 2000:
+    B += ((C - 2000) * 0.08)
+    C = (C - (C - 2000))
+if A < 2000:
+    print(f"Isento")
+else:
+    print(f"R$ {B:.2f}")
