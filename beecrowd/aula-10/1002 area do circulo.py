243704bb57ad9dba0c17e4f110b56704d9248868
diamante3novalorant <20261ctb0100049@estudantes.ifpr.edu.br>
2026-06-07 17:06:11 -0300

    Add files via upload

---
+++ beecrowd/aula-10/1002 area do circulo.py
@@ -0,0 +1,20 @@
+'''
+Problema: beecrowd 1002
+Data: 2026.03.07
+Estudante: Rodrigo Magagnin
+'''
+#Objetivo: Calcular o raio de uma esfera
+
+# --- ANÁLISE (LIAC) ---
+#Entrada: um número de ponto flutuante (o raio "r")
+#Processamento: aplicar a fórmula do raio da esfera
+#Saída: exibir no formato "r = valor" com 3 casa decimais
+#float() converte o valor lido para número decimal (ponto flutuante)
+
+# :.4f formata com exatas 4 casas decimais
+
+n = 3.14159
+r = float (input())
+A = (n*(r**2))
+print(f"A={A:.4f}")
+
