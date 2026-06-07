b3e5ff66a111f4d161dd43b9e73ea5eb61bfe8f0
diamante3novalorant <20261ctb0100049@estudantes.ifpr.edu.br>
2026-06-07 16:25:08 -0300

    Update 1007 diferenca.py

--- aula 12/1007 diferenca.py
+++ aula 12/1007 diferenca.py
@@ -1,1 +1,23 @@
+'''
+Problema: beecrowd 1007
+Data: 2026/04/23
+Estudante: Rodrigo Magagnin
+'''
+
+#Objetivo: Ler quatro inteiros e calcular DIFERENÇA = (A * B) - (C * D)
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: quatro valores inteiros A, B, C e D (um por linha)
+# Processamento: diferença entre o produto A*B e o produto C*D
+# Saída: "DIFERENÇA = valor "(inteiro, letras maiúsculas, espaços ao redor do =)
+
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+
+# Calcula a diferença dos produtos conforme a fórmula do enunciado
+dif = (A * B) - (C * D)
+
+print(f"DIFERENCA = {dif}")
 
